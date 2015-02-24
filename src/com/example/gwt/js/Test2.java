e71a546030b19670d25927e2a10660d995f990df
Michaël Zasso <mic.besace@gmail.com>
2015-02-24 11:26:34 +0100

    wrong namespace and missing constructor

--- src/com/example/gwt/js/Test2.java
+++ src/com/example/gwt/js/Test2.java
@@ -5,9 +5,13 @@ import com.google.gwt.core.client.js.JsNamespace;
 import com.google.gwt.core.client.js.JsType;
 
 @JsType
-@JsNamespace("$wnd.actchem")
+@JsNamespace("$wnd.example")
 @JsExport
 public class Test2 {
+	
+	public Test2() {
+		
+	}
 	
 	public String test() {
 		return testImpl();
